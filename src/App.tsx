e2f36f72184c72a8f5aae39090f84e88df0c7505
Aleksandra Sypula <aleksandra.sypula@gmail.com>
2022-05-26 21:21:22 +0200

    Ground work, displaying links

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
 import React from 'react';
 import './App.css';
 import {InputForm} from "./InputForm";
+import {Routes, Route, Link} from "react-router-dom";
+
+export const routesPaths = {
+    about: 'about',
+    contact: 'contact',
+    surprise: 'surprise'
+}
 
 function App() {
   return (
       <div className="App">
-        <header className="App-header">
-          <InputForm defaultValue={'Type here'}/>
-        </header>
+          <div className='App-header'>
+              <div>
+                  logo
+              </div>
+              <div>
+                  <Link className='App-link' to={'/'}>Home</Link>
+                  <Link className='App-link' to={`/${routesPaths.about}`}>About</Link>
+                  <Link className='App-link' to={`/${routesPaths.contact}`}>Contact</Link>
+                  <Link className='App-link' to={`/${routesPaths.surprise}`}>Surprise</Link>
+              </div>
+          </div>
+          <Routes>
+              <Route path='/' element={<InputForm defaultValue={'Welcome :) '}/>}/>
+              <Route path='*' element={<div>404</div>}/>
+              <Route path={`/${routesPaths.about}`} element={<div>About</div>}/>
+              <Route path={`/${routesPaths.contact}`} element={<div>About</div>}/>
+              <Route path={`/${routesPaths.surprise}`} element={<div>Form</div>}/>
+          </Routes>
       </div>
   );
 }
